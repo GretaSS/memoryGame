01798e764a5c439a1784f89a1efe5395aaa72aa7
GretaSS <gsimonyan228@gmail.com>
2021-02-27 17:55:32 +0400

    fixed cards flipping bugs

--- src/components/GameOn/index.js
+++ src/components/GameOn/index.js
@@ -3,10 +3,8 @@ import React, {
     useState, 
     useEffect
 } from "react";
-import {
-    View
-}from '../../styled';
-import {Context} from '../../Store';
+import { View }from '../../styled';
+import { Context } from '../../Store';
 import styled from 'styled-components';
 
 import cardImages from './cards';
@@ -15,20 +13,17 @@ const Item = styled(View)`
     width: 100px;
     height: 120px;
     margin: 5px 5px 0 0;
-    ${ (props) => props.isFlipped === false ? `		background: linear-gradient(45deg, #1fa5ff 25%, #1053ff 25%, #1053ff 50%, #1fa5ff 50%, #1fa5ff 75%, #1053ff 75%, #1053ff 100%);
+    ${ (props) => props.isFlipped === false ? `background: linear-gradient(0deg, rgba(255,33,33,1) 25%, rgba(252,164,41,1) 74%);
     ` : `transform: rotateY(180deg);` }
 `;
 
 function GameOn(){
     const [state, dispatch] = useContext(Context);
-
     const pictures = cardImages.slice();
     pictures.length = state.height * state.width;
     pictures.push(...pictures);
-    pictures.sort(()=> Math.random() - 0.5);
 
     function shuffle(c){
-        c.length=state.height * state.width;
         return c.sort(()=> Math.random() - 0.5);
     }
 
@@ -38,18 +33,17 @@ function GameOn(){
             imageURL: '/dist/fonts/' + imageURL,
             isFlipped: false,
             canFlip: true
-        }))
+        }));
         return shuffle(c)
     }
+    // useEffect(()=> {
+        
+    // },[])
 
-    ///////////////game begins//////////////
+    //////////////////////////////////////////////////////
     const [cards, setCards] = useState(generate(pictures));
-    // const [canFlip, setCanFlip] = useState(false);
-	// const [firstCard, setFirstCard] = useState(null);
-	// const [secondCard, setSecondCard] = useState(null);
-
-    let firstCard = null;
-    let secondCard = null;
+    const [firstCard, setFirstCard] = useState(null);
+	const [secondCard, setSecondCard] = useState(null);
 
     function setCardIsFlipped(cardId, isFlipped) {
 		setCards(prev => prev.map(c => {
@@ -71,10 +65,10 @@ function GameOn(){
         console.log('success');
         setCardCanFlip(firstCard.id, false);
         setCardCanFlip(secondCard.id, false);
-        setCardIsFlipped(firstCard.id, false);
-		setCardIsFlipped(secondCard.id, false);
-        firstCard = null;
-        secondCard = null;
+        setCardIsFlipped(firstCard.id, true);
+		setCardIsFlipped(secondCard.id, true);
+        setFirstCard(null);
+        setSecondCard(null);
     }
 
     function onFailureGuess() {
@@ -83,24 +77,27 @@ function GameOn(){
         const secondId = secondCard.id;
 
         setTimeout(() => {
-			setCardIsFlipped(firstId, true);
+			setCardIsFlipped(firstId, false);
 		}, 1000);
 		setTimeout(() => {
-			setCardIsFlipped(secondId, true);
+			setCardIsFlipped(secondId, false);
 		}, 1200);
 
-        firstCard = null;
-        secondCard = null;
+        setFirstCard(null);
+        setSecondCard(null);
     } 
 
+    useEffect(() => {
+		if (!firstCard || !secondCard)
+			return;
+		((firstCard.imageURL === secondCard.imageURL) && (firstCard.id !== secondCard.id)) ? onSuccessGuess() : onFailureGuess();
+	}, [firstCard, secondCard]);
+
     function handleClick(pic) {
-        (firstCard) ? secondCard = pic : firstCard = pic;
-        if(secondCard) { setTimeout(() => check(),750) }
-
-    }
-
-    function check(){
-        ((firstCard.imageURL == secondCard.imageURL) && (firstCard.id !== secondCard.id)) ?  onSuccessGuess() : onFailureGuess();
+		if (!pic.canFlip)
+			return;
+        (firstCard) ? setSecondCard(pic) : setFirstCard(pic);
+        setCardIsFlipped(pic.id, true);
     }
 
     return(
