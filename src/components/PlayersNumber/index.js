175d1b1a246cfabcb846cfa6564d56821638c0f7
GretaSS <gsimonyan228@gmail.com>
2021-03-04 21:51:03 +0400

    created the activePlayer func

--- src/components/PlayersNumber/index.js
+++ src/components/PlayersNumber/index.js
@@ -9,6 +9,13 @@ function PlayersNumber(){
 
     let plArr = ["A","B","C","D"];
     plArr.length = state.plValue;
+
+    // let activePlayer = () => {
+    //     dispatch({
+    //         type: 'SET_CURRENT_PLAYER',
+    //         current: plArr[1]
+    //     });
+    // };
     
     return(
         <View id='playersNumber' flex justC='space-between' m='0 auto' w='70%'>
@@ -16,13 +23,13 @@ function PlayersNumber(){
                 {
                     plArr.map((num, key) => 
                         <View key={key}>
-                            Player {num} : 
+                            Player {num} : 0
                         </View>
                     )
                 }
             </View>
             <View>
-                Current Player: 
+                Current Player:  {state.current}
             </View>
         </View>
     )
